e6c5b9cc316f35f0f46ae55c047387a841f378b1
Oleg <assasina23@mail.ru>
2021-07-08 03:09:09 +0800

    Initial commit

---
+++ src/Vyacheslav/Main.java
@@ -0,0 +1,17 @@
+package Vyacheslav;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String nameV="Вячеслав";
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите Ваше имя:");
+        String name = in.nextLine();
+        if (name.equals(nameV)){
+            System.out.println("Привет, Вячеслав");
+        } else {
+            System.out.println("Нет такого имени");
+        }
+    }
+}
